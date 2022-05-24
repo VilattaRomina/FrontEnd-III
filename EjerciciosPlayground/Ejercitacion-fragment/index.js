f67ce6bfadfc69d73ac4f12714d2c5ca74b9c740
VilattaRomina <vilattaromina@gmail.com>
2022-05-23 23:31:37 -0300

    clase4

---
+++ EjerciciosPlayground/Ejercitacion-fragment/index.js
@@ -0,0 +1,13 @@
+const Prueba = () => {
+return (
+    <React.Fragment>
+        <h1>Bob Esponja</h1>
+        <p>Patricio Star</p>
+    </React.Fragment>
+);
+}
+
+
+
+ReactDOM.render(<Prueba/>, document.getElementById('root'));
+
