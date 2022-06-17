4ac29151a551a1c2d28fbb36d51fc0c6a6939120
VilattaRomina <vilattaromina@gmail.com>
2022-06-17 10:55:00 -0300

    clase 7,9,12

---
+++ mdt/clase12/mi-app/src/Components/Signin.js
@@ -0,0 +1,7 @@
+import React, {useState} from 'react'
+
+const Signin = ({userInfo}) =>{
+    const [ signIn, setSignIn] = useState({email: "", password: ""});
+    const [show, setShow] = useState(false);
+
+}
---
+++ mdt/clase12/mi-app/src/HomePage.js
@@ -0,0 +1,31 @@
+
+import './App.css';
+
+function Home(){
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <p>Welcome to our site</p>
+      <p>Please, sign in to see all our events.</p>
+      <p>Sign up if you don't have an account.</p>
+    </div>
+  )
+}
+
+const HomePage =() => {
+
+
+
+  return (
+    <div>
+      <div>
+        <button>Home</button>
+        <button>Sing Up</button>
+        <button>Sing In</button>
+      </div>
+     
+    </div>
+  );
+}
+
+export default HomePage;
